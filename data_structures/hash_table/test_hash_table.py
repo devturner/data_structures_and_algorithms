78c4155e58df54760da4d35e232b444fb23e1548
chris turner <-v>
2018-09-29 16:45:31 -0700

    ht working
    - [x] set, get, delete
    - [x] added tests

--- data_structures/hash_table/test_hash_table.py
+++ data_structures/hash_table/test_hash_table.py
@@ -7,3 +7,42 @@ def test_hash_table_exists():
     """
     assert HashTable
 
+
+def test_add_one():
+    """Test add from ht"""
+    ht = HashTable()
+    ht.set('one', 'item')
+    assert ht.entries_count ==1
+
+
+def test_add_mult():
+    """Test add from ht"""
+    ht = HashTable()
+    ht.set('one', 'item')
+    ht.set('one', 'items')
+    ht.set('two', 'item')
+    assert ht.entries_count == 2
+
+
+def test_get():
+    """Test get from ht"""
+    ht = HashTable()
+    ht.set('one', 'item')
+    ht.set('one', 'items')
+    ht.set('two', 'ITEM')
+    assert ht.get('one') == [['one', 'item', 'items']]
+
+
+def test_remove():
+    """Test remove from ht"""
+    ht = HashTable()
+    ht.set('one', 'item')
+    ht.set('two', 'ITEM')
+    assert ht.remove('one') == ['one', 'item']
+    assert ht.entries_count == 1
+
+
+def test_remove_not_found():
+    """Test remove from ht"""
+    ht = HashTable()
+    assert ht.remove('one') == 'Key not found'
