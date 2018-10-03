a2a9c9245a2dc73620b483e5cfa8f0d5577c56d0
chris turner <-v>
2018-10-02 21:31:45 -0700

    quick sort
    - [x] quick sort method
    - [x] tests
    - [x] whiteboard/ readme (at the main level)

--- sorting_algos/test_sorts.py
+++ sorting_algos/test_sorts.py
@@ -1,5 +1,6 @@
 from .selection import selection_sort
 from .merge import merge_sort
+from .quicksort import quicksort
 from random import shuffle
 import pytest
 
@@ -63,4 +64,26 @@ def test_sorts_list_of_neg_merge_sort():
     unsorted = [-9, 4, 4, -5, 3, 2, 3, 4, 3, 4, 5]
     expected = [-9, -5, 2, 3, 3, 3, 4, 4, 4, 4, 5]
     now_sorted = merge_sort(unsorted)
+    assert expected == now_sorted
+
+
+def test_shuffled_list_gets_sorted_quick_sort():
+    expected = [num for num in range(20)]
+    unsorted = expected[:]
+    shuffle(unsorted)
+    now_sorted = quicksort(unsorted, 0, len(unsorted) - 1)
+    assert expected == now_sorted
+
+
+def test_sorts_list_of_neg_quick_sort():
+    unsorted = [-9, 4, 4, -5, 3, 2, 3, 4, 3, 4, 5]
+    expected = [-9, -5, 2, 3, 3, 3, 4, 4, 4, 4, 5]
+    now_sorted = quicksort(unsorted, 0, len(unsorted) - 1)
+    assert expected == now_sorted
+
+
+def test_sorts_list_of_duplicates_quick_sort():
+    unsorted = [4, 4, 5, 3, 2, 3, 4, 3, 4, 5]
+    expected = [2, 3, 3, 3, 4, 4, 4, 4, 5, 5]
+    now_sorted = quicksort(unsorted, 0, len(unsorted) - 1)
     assert expected == now_sorted
