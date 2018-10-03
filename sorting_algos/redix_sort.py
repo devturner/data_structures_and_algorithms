e128df3cefed56b927c0adb27a41431f02168337
chris turner <-v>
2018-10-03 14:40:18 -0700

    redix sort
    - [x] redix sort
    - [x] test
    - [x] readme

---
+++ sorting_algos/redix_sort.py
@@ -0,0 +1,34 @@
+
+
+def redix_sort(unsorted):
+    """Sort a list of integers by radix algorithm.
+    """
+    base = 10
+    max_length = False
+    temp, current = -1, 1
+
+    if len(unsorted) < 2:
+        return
+
+    while max_length is False:
+        max_length = True
+        buckets = [[] for i in range(base)]
+
+        for i in unsorted:
+            temp = i / current
+            buckets[int(temp % base)].append(i)
+
+            if max_length and temp > 0:
+                max_length = False
+
+        counter = 0
+
+        for i in range(base):
+            sorted = buckets[i]
+
+            for n in sorted:
+                unsorted[counter] = n
+                counter += 1
+        current = current * base
+
+    return unsorted
